0486bccced7f50f31b3d29512156a9e10a2a32d0
pospi <pospi@spadgos.com>
2014-08-13 01:14:20 +1000

    hookup stats

--- stats.js
+++ stats.js
@@ -1,8 +1,17 @@
-module.exports = function(req, res) {
-	var stats = {
-		views: Math.ceil(Math.random()*5000),
-		calls: Math.ceil(Math.random()*5000),
-		emails: Math.ceil(Math.random()*5000)
-	};
-    res.jsonp(stats);
+var mongo = require(__dirname + '/lib/database');
+
+module.exports = function(req, response) {
+	mongo.get().then(function(db) {
+		db.collection('log_totals').find({_id : 'overall_totals'}, function(err, res) {
+			if (err) {
+				response.jsonp(false);
+				throw err;
+			}
+			res.toArray(function(err, res) {
+				response.jsonp(res[0]);
+			});
+		});
+	}, function(err) {
+		throw err;
+	});
 };
