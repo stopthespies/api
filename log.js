3036a5a3a0faf26df00fc704fca0fb1c2f72d8b8
Nathan Craike <ncraike@gmail.com>
2014-08-13 00:40:00 +1000

    Updated /log route to increment event totals
    Previous code added a new document per event. This now increments fields on a single "overall_totals" document.
    
    Next: adding similar documents for each legislator, with separate totals for each.

--- log.js
+++ log.js
@@ -3,6 +3,8 @@ module.exports = function() {
     var MongoClient = require('mongodb').MongoClient;
 
     var EVENT_TYPES = ['view', 'call', 'email'];
+    var COLLECTION_NAME = 'log_totals';
+    var OVERALL_TOTALS_ID = 'overall_totals';
 
     function connectToMongo(onSuccess) {
         MongoClient.connect(
@@ -24,17 +26,22 @@ module.exports = function() {
     }
 
     function sendToMongo(event_type, db) {
-        var new_doc = {"event_type": event_type};
 
-        db.collection("log_totals").insert(
-                new_doc,
-                function(err, result) {
-                    if (err) {
-                        throw err;
-                    }
+        query = {_id: OVERALL_TOTALS_ID};
 
-                    console.log("Added event to MongoDB");
+        inc_update = {$inc: {} };
+        inc_update['$inc'][event_type] = 1;
+
+        db.collection(COLLECTION_NAME).update(
+            query,
+            inc_update,
+            function(err, result) {
+                if (err) {
+                    throw err;
                 }
+
+                console.log("Added event to MongoDB");
+            }
         );
     }
 
