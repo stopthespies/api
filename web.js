581223e83ec767a71a9c3a1dec258f106a701a95
Roland Sharp <rolandnsharp@gmail.com>
2014-08-12 21:03:34 +1000

    add mongo

--- web.js
+++ web.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 var log = require('./log');
 var tweets = require('./tweets');
 
-app.get('/', function(req, res) {
-    res.send({
-        message: 'hello world.'
+
+MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+    if (err) throw err;
+    var totalsCollection = db.collection('totals');
+    var tweetsCollection = db.collection('tweets');
+
+    app.get('/', function(req, res) {
+        res.send({
+            message: 'hello world.'
+        });
     });
-});
 
-app.get('/log', log);
+    app.get('/log', log);
 
-app.get('/tweets', tweets);
+    app.get('/tweets', tweets);
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log('Listening on ' + port);
+    var port = Number(process.env.PORT || 5000);
+    app.listen(port, function() {
+        console.log('Listening on ' + port);
+    });
 });
