c23348bb35aa0ea6cb5742c5f168f0a6b19a85e2
Nathan Craike <ncraike@gmail.com>
2014-08-13 01:01:02 +1000

    Updated /log route to use new MongoDB connection handling.

--- log.js
+++ log.js
@@ -1,29 +1,10 @@
 module.exports = function() {
 
-    var MongoClient = require('mongodb').MongoClient;
+    var mongo = require(__dirname + '/lib/database');
 
     var EVENT_TYPES = ['view', 'call', 'email'];
     var COLLECTION_NAME = 'log_totals';
     var OVERALL_TOTALS_ID = 'overall_totals';
-
-    function connectToMongo(onSuccess) {
-        MongoClient.connect(
-            process.env.MONGOHQ_URL,
-            function onConnect(err, db) {
-                if (err) {
-                    throw err;
-                }
-                console.log("Connected to MongoDB...");
-
-                process.nextTick(
-                    function() {
-                        onSuccess(db);
-                    }
-                );
-            }
-        );
-
-    }
 
     function sendToMongo(event_type, db) {
 
@@ -64,10 +45,14 @@ module.exports = function() {
         if (EVENT_TYPES.indexOf(event_type) > -1) {
             console.log("LOG A "+event_type+" EVENT");
 
-            connectToMongo(
+            mongo.get().then(
                 function onSuccess(db) {
                     sendToMongo(event_type, db);
                     success(res);
+                },
+                function onErr(err) {
+                    fail(res);
+                    throw err;
                 }
             );
             return;
