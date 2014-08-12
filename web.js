336f797632294fdbaaf872cc20032f24a19eab1a
Roland Sharp <rolandnsharp@gmail.com>
2014-08-12 20:51:48 +1000

    added log and tweets get routes

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+
+var log = require('./log');
+var tweets = require('./tweets');
+
 app.get('/', function(req, res) {
     res.send({
         message: 'hello world.'
     });
 });
+
+app.get('/log', log);
+
+app.get('/tweets', tweets);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
