3dc472502921edae467cde079c368749cacc8497
pospi <pospi@spadgos.com>
2014-08-14 19:52:10 +1000

    implement remaining parameters for logging endpoint

--- log.js
+++ log.js
@@ -1,16 +1,39 @@
+/**
+ * Querystring parameters:
+ *
+ * event - The type of event to log (views, calls or emails). Other parameters differ by event type:
+ *
+ * 'views'
+ * 	- can be passed parameterless to register a pageview
+ *  - can come with a 'legislators' comma-separated string of legislator IDs
+ *
+ * 'calls'
+ * 	- must come with a 'legislators' parameter containing only a single ID
+ *
+ * 'emails'
+ * 	- must come with a 'legislators' parameter containing only a single ID
+ *
+ * 'tweets'
+ * 	- must come with a 'legislators' parameter containing only a single ID
+ *
+ * 'facebooks'
+ * 	- must come with a 'legislators' parameter containing only a single ID
+ */
+
+var async = require('async');
+
+var mongo = require(__dirname + '/lib/database');
+var config = require(__dirname + '/_config_');
+
+var EVENT_TYPES = ['views', 'calls', 'emails', 'tweets', 'facebooks'];
+var COLLECTION_NAME = 'log_totals';
+var OVERALL_TOTALS_ID = 'overall_totals';
+
 module.exports = function() {
 
-    var mongo = require(__dirname + '/lib/database');
+    function sendToMongo(query, event_type, db) {
 
-    var EVENT_TYPES = ['views', 'calls', 'emails'];
-    var COLLECTION_NAME = 'log_totals';
-    var OVERALL_TOTALS_ID = 'overall_totals';
-
-    function sendToMongo(event_type, db) {
-
-        query = {_id: OVERALL_TOTALS_ID};
-
-        inc_update = {$inc: {} };
+        var inc_update = {$inc: {} };
         inc_update['$inc'][event_type] = 1;
 
         db.collection(COLLECTION_NAME).update(
@@ -18,7 +41,8 @@ module.exports = function() {
             inc_update,
             function(err, result) {
                 if (err) {
-                    throw err;
+                    console.warn("Mongo write failed:", query, inc_update);
+                    return;
                 }
 
                 console.log("Added event to MongoDB");
@@ -34,33 +58,59 @@ module.exports = function() {
 
     function fail(res) {
         res.jsonp({
-            message: 'Failed to log'
+            error: 'Failed to log'
+        });
+    }
+
+    function invalidRequest(res) {
+        res.jsonp({
+            error: 'Invalid request'
         });
     }
 
     function main(req, res) {
 
         var event_type = req.query.event;
+        var legislators = req.query.legislators || null;
+        var query;
 
-        if (EVENT_TYPES.indexOf(event_type) > -1) {
-            console.log("LOG A "+event_type+" EVENT");
-
-            mongo.get().then(
-                function onSuccess(db) {
-                    sendToMongo(event_type, db);
-                    success(res);
-                },
-                function onErr(err) {
-                    fail(res);
-                    throw err;
-                }
-            );
-            return;
+        if (EVENT_TYPES.indexOf(event_type) == -1) {
+        	console.warn("HACK ATTEMPT:", req);	// let Forever catch these
+	        invalidRequest(res);
+        	return;
         }
 
-        fail(res);
-        return;
+        try {
+	        switch (event_type) {
+	        	case 'views':
+		        	if (legislators) {
+		        		query = { _id : { $in : legislators.split(',') } };
+		        	} else {
+		        		query = { _id : OVERALL_TOTALS_ID };
+		        	}
+	       			break;
+	        	default:
+	        		query = { _id : legislators };
+	       			break;
+	        }
+		} catch (e) {
+			// validation error.
+        	console.warn("HACK ATTEMPT:", req);	// let Forever catch these
+	        invalidRequest(res);
+	        return;
+		}
 
+        console.log("LOG A "+event_type+" EVENT", query);
+
+        mongo.get().then(
+            function onSuccess(db) {
+                sendToMongo(query, event_type, db);
+                success(res);	// do not wait for write acknowledgement
+            },
+            function onErr(err) {
+                fail(res);
+            }
+        );
     };
 
     return main;
