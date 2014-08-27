62978802b3f318bd6cc188453f7ea09ff97d13bc
pospi <pospi@spadgos.com>
2014-08-28 00:09:29 +1000

    extend stats endpoint to allow pulling statistics for particular legislators

--- routes/stats.js
+++ routes/stats.js
@@ -1,15 +1,25 @@
 var mongo = require(__dirname + '/../lib/database');
 
 module.exports = function(req) {
+	var search_record = {_id : 'overall_totals'},
+		response_event = 'get_stats';
+
+	if (req.data && req.data.legislators) {
+		search_record = {_id : {$in : req.data.legislators}};
+		response_event = 'legislator_stats';
+	}
+
 	mongo.get().then(function(db) {
-		db.collection('log_totals').find({_id : 'overall_totals'}, function(err, res) {
+		db.collection('log_totals').find(search_record, function(err, res) {
 			if (err) {
-				req.io.respond(false);
+				req.io.emit(response_event, false);
+				// req.io.respond(false);	// :TODO: check how to return data for both normal HTTP request and socket
 				console.warn("Error reading mongo (totals):", err);
 				return;
 			}
 			res.toArray(function(err, res) {
-				req.io.respond(res[0]);
+				req.io.emit(response_event, res);
+				// req.io.respond(res[0]);
 			});
 		});
 	}, function(err) {
