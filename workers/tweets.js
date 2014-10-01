4f77b59d03584d87e7e77735cdc6829eba43f73f
pospi <pospi@spadgos.com>
2014-10-01 22:59:30 +1000

    parse twitter streaming API in addition to search API to ensure maximum retention

--- workers/tweets.js
+++ workers/tweets.js
@@ -5,16 +5,62 @@ var config = require(__dirname + '/../_config_');
 
 var worker = require('tweets-mongodb-parser');
 
+var Twit = require('twit');
+
+function parseTwitterDate(text)
+{
+	return new Date(Date.parse(text.replace(/( +)/, ' UTC$1')));
+}
 
 mongo.get().then(function(db) {
     process.nextTick(function() {
+
+    	// poll search API
+
     	new worker(db, {
     		creds : config.twitterCreds,
-    		match : config.tweet_processor_match,
+    		match : config.tweet_processor_match.join(' OR '),
     		check_interval : config.tweet_processor_interval,
     		batch_size : config.tweet_processor_batch_size,
     		account_blacklist : config.tweet_processor_account_blacklist,
     	}).start();
+
+    	// watch stream API as well, to scrape up anything the search API misses
+
+    	var twit = new Twit(config.twitterCreds);
+		var stream = twit.stream('statuses/filter', { track : config.tweet_processor_match.join(',') });
+		var coll = db.collection('tweets');
+
+		var u, tweet;
+
+		stream.on('tweet', function(status) {
+			u = status.user;
+			tweet = {
+				_id : status.id_str,
+				id_str: status.id_str,
+				created_at : parseTwitterDate(status.created_at),
+				text : status.text,
+				user : {
+					id : u.id,
+					name : u.name,
+					screen_name : u.screen_name,
+					location : u.location,
+					description : u.description,
+					url : u.url,
+					followers_count : u.followers_count,
+					friends_count : u.friends_count,
+					time_zone : u.time_zone,
+					profile_image_url : u.profile_image_url.replace(/^https?:/i, ''),
+				}
+			};
+
+			coll.save(tweet, { w : 0 });
+		});
+
+		stream.on('limit', function(message) {
+			// log something if we are rate limited. The client will adjust automatically to compensate.
+			console.warn('Exceeded stream limit - missed ' + message.limit.track + ' tweets');
+		});
     });
 }, function(err) {
 	throw err;
