e86b55150ccd71d1840bcd083243ede6003fbaff
pospi <pospi@spadgos.com>
2014-10-04 16:19:06 +1000

    only broadcast stats deltas to save bandwidth and share caches with query endpoint

--- routes/broadcast/logs.js
+++ routes/broadcast/logs.js
@@ -12,44 +12,56 @@
 var async = require('async');
 var mongo = require(__dirname + '/../../lib/database');
 
-var theTotals = null;
+var StatCache = require(__dirname + '/../../lib/stats-cache');
 
-function send(app, totals)
+function send(app, changes)
 {
 	console.log('Broadcast stats');
-	app.io.broadcast('stats:update', totals);
-
-	theTotals = totals;
+	app.io.broadcast('stats:update', changes);
 }
 
 module.exports = function(app, delay)
 {
   mongo.get().then(function(db) {
+
     setTimeout(function pollStats() {
       db.collection('log_totals').find({}, {sort: '_id'}, function(err, res) {
         if (err) throw err;
+
         res.toArray(function(err, totals) {
           if (err) throw err;
 
+          var theTotals = StatCache.get();
+          StatCache.set(totals);
+
+          // if nothing cached yet, broadcast all
           if (!theTotals) {
 	          send(app, totals);
           } else {
-	          for (var i = 0, l = totals.length; i < l; ++i) {
-	          	if (theTotals[i].visits !== totals[i].visits ||
-	          	  theTotals[i].views !== totals[i].views ||
-	              theTotals[i].emails !== totals[i].emails ||
-	              theTotals[i].calls !== totals[i].calls ||
-	              theTotals[i].tweets !== totals[i].tweets ||
-	              theTotals[i].facebooks !== totals[i].facebooks) {
-	          		send(app, totals);
-		          	break;
-	          	}
-	          }
-	      }
+            // otherwise broadcast deltas
+            var changed = [];
 
+            for (var i = 0, l = totals.length; i < l; ++i) {
+            	if (theTotals[i].visits !== totals[i].visits ||
+            	  theTotals[i].views !== totals[i].views ||
+                theTotals[i].emails !== totals[i].emails ||
+                theTotals[i].calls !== totals[i].calls ||
+                theTotals[i].tweets !== totals[i].tweets ||
+                theTotals[i].facebooks !== totals[i].facebooks) {
+                changed.push(totals[i]);
+              }
+            }
+
+            if (changed.length) {
+          		send(app, changed);
+            }
+          }
+
+          // poll for changes
           setTimeout(pollStats, delay);
         });
       });
+
     }, delay);
   });
 };
--- routes/stats.js
+++ routes/stats.js
@@ -1,10 +1,17 @@
 var mongo = require(__dirname + '/../lib/database');
 
+var StatCache = require(__dirname + '/../lib/stats-cache');
+
 module.exports = function(req) {
 	var search_record = {};		// load all by default. There aren't that many members.
 
+	var cached = StatCache.get();
+
 	if (this.input(req, 'legislators')) {
 		search_record = {_id : {$in : this.input(req, 'legislators')}};
+	} else if (cached) {
+		req.io.respond(cached);
+		return;
 	}
 
 	mongo.get().then(function(db) {
